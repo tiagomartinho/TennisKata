96a4ba40e8f269139749ffe511824f20020b3a9e
Tiago Martinho <martinho@thoughtworks.com>
2017-11-04 11:00:15 +0100

    Initial Commit

---
+++ TennisKata/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TennisKata
+//
+//  Created by Tiago Martinho on 4/11/17.
+//  Copyright © 2017 Tiago Martinho. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
