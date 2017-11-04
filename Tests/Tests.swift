7c839aa08c3c85658884aa1f49a6174281203a31
Tiago Martinho <martinho@thoughtworks.com>
2017-11-04 11:29:07 +0100

    adds receiverWinsPoint

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -3,13 +3,18 @@ import XCTest
 class Game {
     
     private var serverPoints = 0
-    
+    private var receiverPoints = 0
+
     var score: String {
-        return "\(serverPoints):0"
+        return "\(serverPoints):\(receiverPoints)"
     }
     
     func serverWinsPoint() {
         serverPoints += 15
+    }
+    
+    func receiverWinsPoint() {
+        receiverPoints += 15
     }
 }
 
@@ -27,5 +32,13 @@ class Tests: XCTestCase {
         game.serverWinsPoint()
 
         XCTAssertEqual("15:0", game.score)
+    }
+    
+    func testWhenReceiverWinsPoint() {
+        let game = Game()
+        
+        game.receiverWinsPoint()
+        
+        XCTAssertEqual("0:15", game.score)
     }
 }
